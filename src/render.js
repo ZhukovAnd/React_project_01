c6167bd62219d601511e25c737af23e4c96581ba
ZhukovAnd <razborkannsklad@gmail.com>
2023-02-16 23:52:36 +0300

    импортированная функция в state.js  не работает

---
+++ src/render.js
@@ -0,0 +1,78 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import {BrowserRouter} from "react-router-dom";
+// //import state from './redux/state';
+// import {addPost} from './redux/state';
+
+// export let rerenderEntireTree = (state) => {
+//   // const root = ReactDOM.createRoot(document.getElementById('root'));
+//   // debugger;
+//    ReactDOM.root.render(
+//    // ReactDOM.render(
+//   <React.StrictMode>
+//    <BrowserRouter>
+//      <App state={state} addPost={addPost} />
+//    </BrowserRouter>
+//  </React.StrictMode>
+ 
+//  );
+//  };
+// // export let rerenderEntireTree = (state) => {
+  
+// //     <React.StrictMode>
+      
+// //       ReactDOM.render(
+// //           <App state={state} addPost={addPost}/>,
+// //        document.getElementById('root')    ); 
+     
+// //      </React.StrictMode>
+      
+// // }
