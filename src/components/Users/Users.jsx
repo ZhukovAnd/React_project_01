f2a7d29d78bf43c5216e66ff96e2021708712199
ZhukovAnd <razborkannsklad@gmail.com>
2023-03-06 00:00:07 +0300

    Users as a class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,47 +1,47 @@
 import React from 'react';
 import style from './Users.module.css';
-
-const Users = (props) => {
-
-if(props.users.length === 0) {
-    props.setUsers([
-        { id: "1", fullName: "Андрей", photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS77J2zptN4syr9RQF3D9TlnxTlejfDMRIjgQ&usqp=CAU',
-        followed: true, status: "Я учу ReactJS", location: {city: "Н. Новгород", country: "Россия"} },
-        { id: "2", fullName: "Василий", photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEf6q6w-L7zbe-bzF1NjxhNLYgf75eNCqgZDzvKz5i7UcQncYuV5w86n9Jq5-mlbWUqy8&usqp=CAU',
-        followed: true, status: "Я учу только JS", location: {city: "Воронеж", country: "Россия"} },
-        { id: "3", fullName: "Дмитрий", photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg6OvFyfXV0svf76GmFpNJlF6dujkLlOym8Q&usqp=CAU',
-        followed: true, status: "Я ни чего не учу, но всё знаю!", location: {city: "Москва", country: "Россия"} },
-        { id: "4", fullName: "Сергей", photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQcLaCtoNMapvv9NdXzqONPXSCoX9Sk8aEQA&usqp=CAU',
-        followed: true, status: "Я специалист по 1С", location: {city: "Казань", country: "Россия"} }
-    ])
-}
+import axios from 'axios';
+import userPhoto from '../../assets/images/images.png'
 
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
-                <span>
-                    <div>
-                        <img src={u.photoUrl} className={style.userPhoto} />
-                    </div>
-                    <div>{ u.followed
-                            ?<button onClick={ () => { props.unfollow(u.id) } } >отписаться</button>
-                            :<button onClick={ () => { props.follow(u.id) } } >подписаться</button>
-                        }
-                    </div>
-                </span>
-                <span>
+class Users extends React.Component {
+    constructor(props) {
+        super(props)
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id} >
                     <span>
-                        <div>{u.fullName}</div>
-                        <div>{u.status}</div>
+                        <div>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={style.userPhoto} alt="Avatar" />
+                        </div>
+                        <div>{u.followed
+                            ? <button onClick={() => { this.props.unfollow(u.id) }} >отписаться</button>
+                            : <button onClick={() => { this.props.follow(u.id) }} >подписаться</button>
+                        }
+                        </div>
                     </span>
-                </span>
-                <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
-                </span>
-            </div>)
-        }
-    </div>
+                    <span>
+                        <span>
+                            <div>{u.name}</div>
+                            <div>{u.status}</div>
+                        </span>
+                    </span>
+                    <span>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
+                    </span>
+                </div>)
+            }
+        </div>
+    }
 }
+
+
 export default Users;
---
+++ src/components/Users/UsersOld.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import style from './Users.module.css';
+import axios from 'axios';
+import userPhoto from '../../assets/images/images.png'
+
+
+const Users = (props) => {
+ 
+if(props.users.length === 0) {
+    // props.setUsers([
+    //     { id: "1", fullName: "Андрей", photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS77J2zptN4syr9RQF3D9TlnxTlejfDMRIjgQ&usqp=CAU',
+    //     followed: true, status: "Я учу ReactJS", location: {city: "Н. Новгород", country: "Россия"} },
+    //     { id: "2", fullName: "Василий", photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEf6q6w-L7zbe-bzF1NjxhNLYgf75eNCqgZDzvKz5i7UcQncYuV5w86n9Jq5-mlbWUqy8&usqp=CAU',
+    //     followed: true, status: "Я учу только JS", location: {city: "Воронеж", country: "Россия"} },
+    //     { id: "3", fullName: "Дмитрий", photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg6OvFyfXV0svf76GmFpNJlF6dujkLlOym8Q&usqp=CAU',
+    //     followed: true, status: "Я ни чего не учу, но всё знаю!", location: {city: "Москва", country: "Россия"} },
+    //     { id: "4", fullName: "Сергей", photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQcLaCtoNMapvv9NdXzqONPXSCoX9Sk8aEQA&usqp=CAU',
+    //     followed: true, status: "Я специалист по 1С", location: {city: "Казань", country: "Россия"} }
+    // ])
+
+//"https://social-network.samuraijs.com/api/1.0/users"
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+          
+            props.setUsers(response.data.items)
+        })
+
+}
+
+
+
+    return <div>
+        {
+            props.users.map(u => <div key={u.id} >
+                <span>
+                    <div>
+
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto } className={style.userPhoto} alt="Avatar" />
+                    </div>
+                    <div>{ u.followed
+                            ?<button onClick={ () => { props.unfollow(u.id) } } >отписаться</button>
+                            :<button onClick={ () => { props.follow(u.id) } } >подписаться</button>
+                        }
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                </span>
+                <span>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
+                </span>
+            </div>)
+        }
+    </div>
+}
+export default Users;
