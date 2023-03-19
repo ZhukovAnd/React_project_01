9fefa028f61bf56031f0956ec21117cd19e42321
ZhukovAnd <56645245+ZhukovAnd@users.noreply.github.com>
2023-03-20 00:15:20 +0300

    In withAuthRedirect async isAuthUser

--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -2,17 +2,25 @@ import React from "react";
 import { connect } from "react-redux";
 import { Navigate } from "react-router-dom";
 
+
 let mapStateToPropsForRedirect = (state) => ({
     isAuth: state.auth.isAuth
 })
 
 export const withAuthRedirect = (Component) => {
+
     class RedirectComponent extends React.Component {
         render() {
+            const isAuthUser = async() => {
+                await this.props.setAuthUserData()
+           
             if(!this.props.isAuth) return <Navigate to='/login' />//redirect to login
+ }
+       isAuthUser()
             return <Component {...this.props} />        
-        }
+        } 
     }
+    
 
 let ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent)
 
