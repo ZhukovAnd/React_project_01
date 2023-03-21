55f5def74c9bfada8675e6533bcdd80c2443a5d9
ZhukovAnd <56645245+ZhukovAnd@users.noreply.github.com>
2023-03-21 19:18:29 +0300

    Create ProfileStatuc using local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './ProfileInfo.module.css';
 import Preloader from '../../AssistantsComponent/Preloader/Preloader';
 import profilePhoto from './../../../assets/images/profilePhoto.jpg'
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -9,15 +10,16 @@ const ProfileInfo = (props) => {
     }
     return (
         <div className={style.content}>
-            <div className='content_img'>
+            {/* <div className='content_img'>
                 <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg' alt="img_content" />
-            </div>
+            </div> */}
             <div>
                 <img src={props.profile.photos.large != null
                 ? props.profile.photos.large
                 : profilePhoto
                 } alt='Avatar' />
             </div>
+            <ProfileStatus status="Hello my friends!"/>
             <div className={style.profileInfo}>
                 <div>Меня зовут: <span> {props.profile.fullName}</span></div>
                 <div>Обо мне: <span> {props.profile.aboutMe}</span></div>
